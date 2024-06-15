d65a04d2a8bb0142e7605c4258e92d19851ba85b
Ankit Kumar <ak0182274@gmail.com>
2024-06-15 10:01:41 +0530

    add file structure-1

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-  className="flex-1 items-center justify-center bg-gray-100"
+  className="flex-1 items-center justify-center bg-white"
     >
-      <Text className="text-2xl font-pblack">Edit app/index.tsx to edit this screen.</Text>
-      <Image
-        source={require("../assets/icons/bookmark.png")}
-        style={{ width: 100, height: 100 }}
-      />
+      <Text className="text-2xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href='/home'>Home</Link>
     </View>
-  );
+  ); 
 }
