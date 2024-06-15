1a04cba01298b5bead6c560e261a492d111cec6c
Ankit Kumar <ak0182274@gmail.com>
2024-06-15 09:52:15 +0530

    set fonts import

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
   className="flex-1 items-center justify-center bg-gray-100"
     >
-      <Text className="text-2xl">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-2xl font-pblack">Edit app/index.tsx to edit this screen.</Text>
+      <Image
+        source={require("../assets/icons/bookmark.png")}
+        style={{ width: 100, height: 100 }}
+      />
     </View>
   );
 }
---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+import icons from "./Icons";
+import images from "./Images";
+
+export { icons, images };
