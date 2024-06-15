7fec1a6ded66c991269b923ee91c2676ce25bd94
Ankit Kumar <ak0182274@gmail.com>
2024-06-15 08:33:00 +0530

    Initialiize with Nativewinf

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+  className="flex-1 items-center justify-center bg-gray-100"
+    >
+      <Text className="text-xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
